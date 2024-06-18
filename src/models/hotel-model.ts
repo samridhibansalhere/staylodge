abbe904ac7924a5ca86568055c383ce82ce3b46d
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2024-06-19 03:33:43 +0530

    Update hotel-model.ts

--- src/models/hotel-model.ts
+++ src/models/hotel-model.ts
@@ -1,41 +1,40 @@
 import mongoose from "mongoose";
-const hotelSchema=new mongoose.Schema({
-    name:{
-        type: String,
-        required: true
+
+const hotelSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    owner:{
-        type: String,
-        required: true
+    owner: {
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: [true, 'Email address is required'],
-        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
+      type: String,
+      required: true,
     },
     phone: {
-        type: String,
-        required: [true, 'Phone number is required'],
-        validate: {
-            validator: function(v) {
-                return /^\d{10}$/.test(v);
-            },
-            message: props => `${props.value} is not a valid 10-digit phone number!`
-        }
+      type: String,
+      required: true,
     },
-    address:{
-        type: String,
-        required: true
+    address: {
+      type: String,
+      required: true,
     },
-    media:{
-        type: Array,
-        required: true
+    media: {
+      type: Array,
+      required: true,
     },
-},{
-    timestamps: true
-});
-if(mongoose.models && mongoose.models["hotels"]){
-    delete mongoose.models["hotels"]
+  },
+  {
+    timestamps: true,
+  }
+);
+
+if (mongoose.models && mongoose.models["hotels"]) {
+  delete mongoose.models["hotels"];
 }
-const HotelModel=mongoose.model("hotels",hotelSchema);
+
+const HotelModel = mongoose.model("hotels", hotelSchema);
 export default HotelModel;
